072cea836912a817c6f42dbf9efa67e1e26a7cf6
Neha-Singhal-0 <nehasinghal032415@gmail.com>
2024-10-19 12:09:51 +0530

    updating index.js and file.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const express = require("express");
 const app = express();
 
+
+
 //PORt find krna h 
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+
+
 
 //middleware add krne h 
 app.use(express.json());
@@ -14,19 +18,28 @@ app.use(fileupload({
     tempFileDir : '/tmp/'
 }));
 
+
 //db se connect krnah 
 const db = require("./config/database");
 db.connect();
+
+
 
 //cloud se connect krna h 
 const cloudinary = require("./config/cloudinary");
 cloudinary.cloudinaryConnect();
 
+
+
 //api route mount krna h 
 const Upload = require("./routes/FileUpload");
 app.use('/api/v1/upload', Upload);
+
+
 
 //activate server
 app.listen(PORT, () => {
     console.log(`App is running at ${PORT}`);
 })
+
+
--- models/File.js
+++ models/File.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 
+
+
 const fileSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -18,11 +20,11 @@ const fileSchema = new mongoose.Schema({
 });
 
 
+
 //post middleware
 fileSchema.post("save", async function(doc) {
     try{
         console.log("DOC",doc)
-
         //transporter 
         let transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
@@ -31,24 +33,18 @@ fileSchema.post("save", async function(doc) {
                 pass: process.env.MAIL_PASS,
             },
         });
-
         //send mail 
         let info = await transporter.sendMail({
             from:`StudyNotion - by Neha`,
             to: doc.email,
             subject: "New File Uploaded on Cloudinary",
             html:`<h2>Hello User!</h2> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
-        })
-        
+        }) 
         console.log("INFO", info);
-
-
     }
     catch(error) {
         console.error(error);
     }
 })
-
-
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
